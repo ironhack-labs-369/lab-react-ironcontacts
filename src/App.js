49ea4487d5c12cf57d6bacaced8cb753da6c217e
mcapurri <m.capurri@gmail.com>
2021-02-24 11:51:35 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import contacts from './contacts.json';
 
 function App() {
-    const displayContacts = contacts.map((contact) => {
-        console.log('contact', contact);
+    const displayContacts = contacts.slice(0, 5).map((contact) => {
         return (
             <>
                 <tr key={contact.id}>
@@ -16,21 +15,29 @@ function App() {
                         />
                     </td>
                     <td>{contact.name}</td>
-                    <td>{contact.popularity}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
                 </tr>
             </>
         );
     });
+    const randomContact = () => {
+        let random =
+            contacts[Math.floor(Math.random() * (5 - contacts.length) + 5)];
+        console.log('random', random);
+    };
     return (
         <div className="App">
             <table>
                 <thead>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th>Popularity</th>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Popularity</th>
+                    </tr>
                 </thead>
                 <tbody>{displayContacts}</tbody>
             </table>
+            <button onClick={randomContact}>Add random contact</button>
         </div>
     );
 }
